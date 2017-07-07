b3bebdd5b088741209982364ec2bcb42a52e0dce
avacr <c18ac@dalton.org>
2017-07-06 20:52:32 -0400

    CurrencyExchange Final

--- currencyExchange/ViewController.swift
+++ currencyExchange/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var dollarAmount: UITextField!
+    @IBOutlet weak var euroAmount: UITextField!
+    @IBAction func convertAction(_ sender: Any) {
+        if let dollar = Double(dollarAmount.text!) {
+            let euro = dollar * 0.88
+            euroAmount.text = String(format: "%.2f", euro)
+        } else {
+            euroAmount.text = ""
+            dollarAmount.text = ""
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
